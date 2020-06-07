99dc0590b0366736043c18df2fb1e499e0c2bd75
maxim.xprt <maxim.xprt@gmail.com>
2020-06-07 22:12:57 +0300

    First prod version

--- airflow101-hw02/hw02.py
+++ airflow101-hw02/hw02.py
@@ -30,14 +30,15 @@ ORDERS_FILENAME_FIXED = 'orders_fixed.csv'
 PAYMENT_STATUS_SRC = 'https://api.jsonbin.io/b/5ed7391379382f568bd22822'
 PAYMENT_STATUS_FILENAME = 'payment_status.csv'
 
+###
+### Utils
+###
 
 def fix_ordersfile_header():
     header = ['id', 'uuid', 'good_name', 'order_date', 'amount', 'customer_name', 'customer_email']
-
     with open(os.path.join(STAGE_DIR, ORDERS_FILENAME), mode='r') as src_file:
         reader = csv.DictReader(src_file, fieldnames=header)
 
-        # use newline='' to avoid adding new CR at end of line
         with open(os.path.join(STAGE_DIR, ORDERS_FILENAME_FIXED), mode='w', newline='') as dest_file: 
             print(dest_file.name)
             writer = csv.DictWriter(dest_file, fieldnames=reader.fieldnames)
@@ -57,79 +58,6 @@ def download_payment_status():
             order_uuid = d
             payment_status = data[d]['success']
             payment_status_writer.writerow([order_uuid, payment_status])
-
-
-sql_create_stage_customers = """
-    drop table if exists stage_customers;
-    create table stage_customers
-            (
-                id integer,
-                name varchar(512),
-                birth_date date not null,
-                gender varchar(1),
-                email varchar(128)
-            );
-    """
-
-sql_create_stage_goods = """
-    drop table if exists stage_goods;
-    create table stage_goods
-        (
-            id integer,
-            name text,
-            price numeric
-        );
-    """
-
-
-sql_create_stage_orders = """
-    drop table if exists stage_orders;
-    create table stage_orders
-    (
-        id integer,
-        uuid uuid,
-        good_name varchar(512),
-        order_date timestamp,
-        amount integer,
-        customer_name varchar(512),
-        customer_email varchar(128)
-    );
-    """
-
-sql_create_stage_payment_status = """
-    drop table if exists stage_payment_status;
-    create table stage_payment_status
-    (
-        order_uuid uuid,
-        status boolean
-    );
-"""
-
-# pg to pg customers
-sql_get_src_customers = """
-    select id,
-            trim(name),
-            birth_date,
-            gender,
-            trim(email)
-    from customers
-"""
-
-sql_insert_target_customers = """
-    insert into stage_customers values %s
-"""
-
-# pg to pg goods
-sql_get_src_goods = """
-    select id,
-            trim(name),
-            price
-    from goods
-"""
-
-sql_insert_target_goods = """
-    insert into stage_goods values %s
-"""
 
 
 def load_pg_to_pg_stage(sql_get_src, sql_insert_target):
@@ -165,6 +93,103 @@ def load_csv_to_pg_stage(csv_dir, csv_filename, pg_tablename):
     src_csv.close()
 
 
+### 
+###  SQL templates
+###
+
+sql_create_stage_customers = """
+    drop table if exists stage_customers;
+    create table stage_customers
+            (
+                id integer,
+                name varchar(512),
+                birth_date date not null,
+                gender varchar(1),
+                email varchar(128)
+            );
+    """
+
+sql_create_stage_goods = """
+    drop table if exists stage_goods;
+    create table stage_goods
+        (
+            id integer,
+            name text,
+            price numeric
+        );
+    """
+
+sql_create_stage_orders = """
+    drop table if exists stage_orders;
+    create table stage_orders
+    (
+        id integer,
+        uuid uuid,
+        good_name varchar(512),
+        order_date timestamp,
+        amount integer,
+        customer_name varchar(512),
+        customer_email varchar(128)
+    );
+    """
+
+sql_create_stage_payment_status = """
+    drop table if exists stage_payment_status;
+    create table stage_payment_status
+    (
+        order_uuid uuid,
+        status boolean
+    );
+"""
+
+# pg to pg customers
+sql_get_src_customers = "select id, trim(name), birth_date, gender, trim(email) from customers"
+sql_insert_target_customers = "insert into stage_customers values %s"
+
+# pg to pg goods
+sql_get_src_goods = "select id, trim(name), price from goods"
+sql_insert_target_goods = "insert into stage_goods values %s"
+
+# Final dataset
+sql_load_final_dataset = """
+    create table if not exists final_dataset
+    (
+        name varchar(512),
+        age double precision,
+        good_title varchar(512),
+        order_date timestamp,
+        payment_status boolean,
+        total_price numeric,
+        amount integer,
+        last_modified_at timestamp with time zone
+    );
+
+    truncate table final_dataset;
+
+    insert into final_dataset(name, age, good_title, order_date, payment_status, total_price, amount, last_modified_at) 
+    select
+            c.name
+            ,date_part('year',age(c.birth_date)) as age
+            ,o.good_name as good_title
+            ,o.order_date
+            ,p.status as payment_status
+            ,o.amount * g.price as total_price
+            ,o.amount
+            ,now() as last_modified_at
+      from stage_orders o
+      left join stage_customers c
+             on trim(upper(c.email)) = trim(upper(o.customer_email))
+      left join stage_goods g
+             on trim(upper(g.name)) = trim(upper(o.good_name))
+      left join stage_payment_status p
+             on p.order_uuid = o.uuid;
+"""
+
+
+
+###
+### DAG
+###
 
 with DAG(dag_id='hw02', default_args=args, schedule_interval=None) as dag:
     # Entry point
@@ -188,27 +213,27 @@ with DAG(dag_id='hw02', default_args=args, schedule_interval=None) as dag:
     )
 
     create_stage_customers = PostgresOperator(
-        task_id='create_stage_customers',
+        task_id = 'create_stage_customers',
         postgres_conn_id = 'postgre_hw02_trg',
-        sql=sql_create_stage_customers
+        sql = sql_create_stage_customers
     )
 
     create_stage_goods = PostgresOperator(
-        task_id='create_stage_goods',
+        task_id = 'create_stage_goods',
         postgres_conn_id = 'postgre_hw02_trg',
-        sql=sql_create_stage_goods
+        sql = sql_create_stage_goods
     )
 
     create_stage_orders = PostgresOperator(
-        task_id='create_stage_orders',
+        task_id = 'create_stage_orders',
         postgres_conn_id = 'postgre_hw02_trg',
-        sql=sql_create_stage_orders
+        sql = sql_create_stage_orders
     )
 
     create_stage_payment_status = PostgresOperator(
         task_id = 'create_stage_payment_status',
         postgres_conn_id = 'postgre_hw02_trg',
-        sql=sql_create_stage_payment_status
+        sql = sql_create_stage_payment_status
     )
 
     load_stage_customers = PythonOperator(
@@ -235,7 +260,11 @@ with DAG(dag_id='hw02', default_args=args, schedule_interval=None) as dag:
         op_kwargs = {'csv_dir': STAGE_DIR, 'csv_filename': PAYMENT_STATUS_FILENAME, 'pg_tablename': 'stage_payment_status'}
     )
 
-    
+    load_final_dataset = PostgresOperator(
+        task_id = 'load_final_dataset',
+        postgres_conn_id = 'postgre_hw02_trg',
+        sql = sql_load_final_dataset
+    )
 
     dummy_task >> [download_orders,
                    download_payment_status,
@@ -243,7 +272,13 @@ with DAG(dag_id='hw02', default_args=args, schedule_interval=None) as dag:
                    create_stage_goods,
                    create_stage_orders,
                    create_stage_payment_status]
-    download_orders >> fix_orders_header >> load_stage_orders
+    [download_orders, create_stage_orders] >> fix_orders_header >> load_stage_orders
     create_stage_customers >> load_stage_customers
     create_stage_goods >> load_stage_goods
-    create_stage_payment_status >> load_stage_payment_status
+    
+    [download_payment_status, create_stage_payment_status] >> load_stage_payment_status
+
+    [load_stage_orders,
+     load_stage_customers,
+     load_stage_goods,
+     load_stage_payment_status] >> load_final_dataset
